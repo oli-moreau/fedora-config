d7ca6b1cb2d033901a39a3a9aaf165c9e5c87dfa
Olivier Moreau <moreau.olivier@outlook.com>
2023-04-22 09:51:14 -0400

    Update to Fedora 38
    - Modify rpmfusion url to fedora 38
    - Add php & composer installation

--- lib/packages.sh
+++ lib/packages.sh
@@ -8,9 +8,9 @@ update_system() {
 
 # Add RPM Fusion and Flathub
 add_repos() {
-    wget -c https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-branched.noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-branched.noarch.rpm
-    sudo dnf install -y ./rpmfusion-free-release-branched.noarch.rpm
-    sudo dnf install -y ./rpmfusion-nonfree-release-branched.noarch.rpm
+    wget -c https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-38.noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-38.noarch.rpm
+    sudo dnf install -y ./rpmfusion-free-release-38.noarch.rpm
+    sudo dnf install -y ./rpmfusion-nonfree-release-38.noarch.rpm
     rm -f rpmfusion-*
     sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 }
@@ -18,7 +18,7 @@ add_repos() {
 # Remove unused packages & install what is needed
 install_packages() {
     sudo dnf remove -y libreoffice* rhythmbox gnome-contacts gnome-weather gnome-maps gnome-tour gnome-boxes gnome-calendar gnome-clocks
-    sudo dnf install -y gnome-tweaks gnome-console transmission-gtk filezilla gcolor3 libreoffice-writer vlc intel-media-driver steam blender
+    sudo dnf install -y gnome-tweaks gnome-console transmission-gtk filezilla gcolor3 libreoffice-writer vlc intel-media-driver steam blender php
     flatpak install -y com.github.GradienceTeam.Gradience sh.cider.Cider com.mattjakeman.ExtensionManager
 }
 
@@ -29,7 +29,7 @@ install_packages_remote() {
     rm google*.rpm
 }
 
-# Install VSCode, GitKraken, NodeJS
+# Install VSCode, GitKraken, NodeJS, Composer
 install_dev_tools() {
     sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
     sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
@@ -41,4 +41,10 @@ install_dev_tools() {
     rm gitkraken*.rpm
 
     sudo dnf install -y nodejs
+
+    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+    php composer-setup.php
+    php -r "unlink('composer-setup.php');"
+    sudo mv composer.phar /usr/bin/composer
 }
